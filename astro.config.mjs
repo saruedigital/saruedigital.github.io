ec2387766bea89c9eba70f9179118066038d6fa3
gustapinto <ghp2201@gmail.com>
2026-08-12 10:25:08 -0300

    feat: add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    outDir: "docs/",
-    site: 'https://saruedigital.github.io',
+  outDir: "docs/",
+  site: 'https://saruedigital.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
